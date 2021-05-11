48427dace4fd35785807d1d20045ca0bf51385cb
Renato Perez <renato.perez@wekynect.com>
2021-05-10 20:53:27 -0600

    Set basic boilerplate for GraQLServer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { testText } from './test';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log({ testText });
+// type definitions
+const typeDefs = `
+  type Query {
+    hello:String!
+    name: String!
+    location: String!
+    bio:String!
+  }
+`;
+// resolvers
+const resolvers = {
+  Query: {
+    hello: (): string => 'This is my first query!',
+    name: (): string => 'Renato Perez',
+    location: (): string => 'Guatemala City',
+    bio: (): string => 'Frontend developer keen into Typescript',
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on localhost:4000'));
--- src/test.ts
+++
@@ -1,1 +0,0 @@
-export const testText = 'imported const';
